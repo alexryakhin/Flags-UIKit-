bd04a442e358d443741156880b494fcb0672a422
Alex Bonney <bonney977@gmail.com>
2021-07-26 07:54:28 +0300

    Initial Commit

---
+++ ChallengeProjectDay23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeProjectDay23
+//
+//  Created by Alexander Bonney on 7/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
